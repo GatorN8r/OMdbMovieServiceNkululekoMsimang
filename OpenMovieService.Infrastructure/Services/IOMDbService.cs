ee1759e8a457d9494d073b398ccebbc05b0b419e
LindokuhleMsimang <LindokuhleMsimang@DESKTOP-76CQHET>
2025-05-20 20:34:41 +0200

    Add project files.

---
+++ OpenMovieService.Infrastructure/Services/IOMDbService.cs
@@ -0,0 +1,16 @@
+﻿using OpenMovieService.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMovieService.Infrastructure.Services
+{
+    public interface IOMDbService
+    {
+        void PopulateRequestHeader();
+        Task<Movie> GetMovieById(string id);
+        Task<Movie> GetMovieByTitle(string name);
+    }
+}
---
+++ OpenMovieService.Infrastructure/Services/OMDbService.cs
@@ -0,0 +1,27 @@
+﻿using OpenMovieService.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMovieService.Infrastructure.Services
+{
+    public class OMDbService : IOMDbService
+    {
+        public Task<Movie> GetMovieById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovieByTitle(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateRequestHeader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
